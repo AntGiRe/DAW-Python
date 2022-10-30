12d7bc0f620e5dc0f3240e67c52ef10cf25e4640
agil-rey <agil-rey@student.42malaga.com>
2022-10-30 08:29:30 +0100

    some corrections in R5

--- Relacion5/e1.py
+++ Relacion5/e1.py
@@ -17,24 +17,24 @@ class Persona():
 		self.nombre = nombre
 	
 	def print_persona(self):
-		return f'{self.nombre} - Edad: {self.edad}'
+		print("El nombre es: " + self.nombre + " y la edad es: " + str(self.edad))
 	
 	def es_mayor_de_edad(self):
-		return True if self.edad > 18 else False
+		return True if self.edad >= 18 else False
 
 	def es_mayor_que(self, persona):
 		if (persona.get_edad() < self.edad):
 			return "Tu eres mayor que esa persona"
-		elif ( self.edad < persona.getedad()):
+		elif ( self.edad < persona.get_edad()):
 			return "Esa persona es mayor que tu"
 		else:
 			return "Teneis la misma edad"
 	
 	@staticmethod
 	def get_mayor(persona1, persona2):
-		if (persona1.getedad() > persona2.get_edad()):
+		if (persona1.get_edad() > persona2.get_edad()):
 			return persona1
-		elif (persona2.get_edad() > persona1.getedad()):
+		elif (persona2.get_edad() > persona1.get_edad()):
 			return persona2
 		else:
 			return None
@@ -43,8 +43,8 @@ class Persona():
 a = Persona("Antonio", 20)
 b = Persona("Amalia", 13)
 
-print(a.print_persona())
-print(b.print_persona())
+a.print_persona()
+b.print_persona()
 
 print("Antonio", a.es_mayor_de_edad())
 print("Amalia", b.es_mayor_de_edad())
@@ -52,4 +52,6 @@ print("Amalia", b.es_mayor_de_edad())
 print(a.es_mayor_que(b), "Antonio")
 print(b.es_mayor_que(a), "Amalia")
 
-print("El mayor es:", Persona.get_mayor(a, b).print_persona())
+print("El mayor es la siguiente persona:");
+p = Persona.get_mayor(a, b)
+p.print_persona()
