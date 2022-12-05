a7f7fb7bac8efa12edf265000900f9efd093645a
agil-rey <agil-rey@student.42malaga.com>
2022-12-05 17:12:44 +0100

    changes on e5

--- Examenes/Examen_1T/e5.py
+++ Examenes/Examen_1T/e5.py
@@ -1,13 +1,13 @@
 """Antonio Jesús Gil Reyes - Ejercicio 5"""
 
-dic = {}
-lis = [];
+mi_dict = {}
 
 while True:
 	#Menu
 	print("\n1. Introduce nueva persona")
-	print("2. Listar personas")
-	print("3. Salir")
+	print("2. Listar personas por nombre")
+	print("3. Listar personas por apellidos")
+	print("4. Salir")
 	opt = int(input("Introduce una opción: "))
 
 	match opt:
@@ -15,14 +15,27 @@ while True:
 			#Se pide un nombre y apellido. Se mete en el diccionario
 			name = input("Introduce un nombre: ")
 			lastname = input("Introduce un apellido: ")
-			dic[name] = lastname
+			mi_dict[name] = lastname
 		case 2:
-			#Sin terminar ordenacion
-			print("Ordenar por:")
-			print("1. Nombre")
-			print("2. Apellido")
-			opt = int(input("Introduce una opción: "))
+			mi_list = list(mi_dict.keys())
+			mi_dict_aux = {}
+			mi_list.sort()
+			for nombre in mi_list:
+				mi_dict_aux[nombre] = mi_dict[nombre]
+			mi_dict = mi_dict_aux
+			print("El diccionario ordenado es: ", mi_dict)
 		case 3:
+			mi_lista_apellidos = list(mi_dict.values())
+			mi_lista_nombres = list(mi_dict.keys())
+			mi_lista_apellidos.sort()
+			mi_dict_aux = {}
+			for apellido in mi_lista_apellidos:
+				for nombre in mi_lista_nombres:
+					if mi_dict[nombre] == apellido:
+						mi_dict_aux[nombre] = apellido
+			mi_dict = mi_dict_aux
+			print("El diccionario ordenado es: ", mi_dict)
+		case 4:
 			#Salir
 			opt = input("¿Está seguro que desea salir (s/N)? ")
 			if opt == "s":
